4770e84d1156c35615d8b12ce4897b9beb2faac0
KrzysztofRozbicki <plkrozbicki@gmail.com>
2023-06-17 17:04:27 +0100

    remove console.logs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/goit-react-hw-02-feedback/',
+  base: 'https://krzysztofrozbicki.github.io/goit-react-hw-02-feedback/',
 });
